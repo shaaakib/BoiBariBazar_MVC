9c88a069c0f94fed1fde8d7b0d25198368136954
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-04-10 12:25:20 +0600

    Implement Category Repository

---
+++ BoiBariBazar.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using BoiBariBazar.DataAccess.Data;
+using BoiBariBazar.DataAccess.Repository.IRepository;
+using BoiBariBazar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoiBariBazar.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
