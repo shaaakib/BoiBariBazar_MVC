d5287abce273536aea6dd28c8fc2a5689ef93dff
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-04-13 11:49:40 +0600

    Product Repository and UnitOfWork

--- BoiBariBazar.DataAccess/Repository/IRepository/IProductRepository.cs
+++ BoiBariBazar.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoiBariBazar.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,8 @@ using System.Threading.Tasks;
 
 namespace BoiBariBazar.DataAccess.Repository.IRepository
 {
-    public interface IUnitOfWork
+    public interface IProductRepository : IRepository<Product>
     {
-        ICategoryRepository Category { get; }
-        IProductRepository Product { get; }
-        void Save();
+        void Update(Product obj);
     }
 }
