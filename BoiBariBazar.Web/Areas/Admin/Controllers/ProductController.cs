a0b3b788fa2346546394c8dbf3de1c6949ef6dea
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-04-13 15:04:07 +0600

    Product : Index and Create logic added

---
+++ BoiBariBazar.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using BoiBariBazar.DataAccess.Repository.IRepository;
+using BoiBariBazar.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoiBariBazar.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> productList = _unitOfWork.Product.GetAll().ToList();
+            return View(productList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+            }
+        }
+}
