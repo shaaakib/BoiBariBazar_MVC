79f5200b059c395f833c6e38389f7c3f3c1a4d84
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-04-17 16:40:20 +0600

    addForeignKeyForCategoryProductRelation

--- BoiBariBazar.Models/Product.cs
+++ BoiBariBazar.Models/Product.cs
@@ -40,6 +40,11 @@ namespace BoiBariBazar.Models
         [Range(1, 1000)]
         public double Price100 { get; set; }
 
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public string ImageUrl { get; set; }
+
     }
 
 }
