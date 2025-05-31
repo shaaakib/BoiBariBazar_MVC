b6017c39a76132a755e7acb7e1ed12c093110d83
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-05-31 21:37:03 +0600

    Details Action Method

--- BoiBariBazarWeb/Areas/Customer/Controllers/HomeController.cs
+++ BoiBariBazarWeb/Areas/Customer/Controllers/HomeController.cs
@@ -24,9 +24,9 @@ namespace BoiBariBazar.Web.Areas.Customer.Controllers
             return View(productList);
         }
 
-        public IActionResult Details(int id)
+        public IActionResult Details(int productId)
         {
-            Product product = _unitOfWork.Product.Get(u => u.Id == id, includeProperties: "Category");
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
             return View(product);
         }
 
