0a7f056165f2ddc8e46d6e30c3f7274c0d65b866
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-06-03 19:28:25 +0600

    Assign Roles on Registration

--- BoiBariBazarWeb/Program.cs
+++ BoiBariBazarWeb/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("BoiBariCon")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
