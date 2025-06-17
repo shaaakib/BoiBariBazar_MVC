e00bac0d2a4f39dfa081d52a314c49e4cc52a07f
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-06-17 20:22:52 +0600

    Add Shopping Cart to Repository

---
+++ BoiBariBazar.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,25 @@
+﻿using BoiBariBazar.DataAccess.Data;
+using BoiBariBazar.DataAccess.Repository.IRepository;
+using BoiBariBazar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoiBariBazar.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
